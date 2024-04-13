d2115cca176ed912bb4129200625dde5ae3df154
Aleksei Arkhangelskii <arhmost@gmail.com>
2024-04-13 12:02:40 +0200

    init: start task 2

---
+++ Task_2/test.ts
@@ -0,0 +1,45 @@
+
+import { chromium, Browser, Page } from 'playwright';
+import * as fs from 'fs';
+
+describe('Website counters', () => {
+  let browser: Browser;
+  let page: Page;
+
+  beforeAll(async () => {
+    browser = await chromium.launch();
+    page = await browser.newPage();
+  }, 10000);
+
+  afterAll(async () => {
+    await browser.close();
+  }, 10000);
+
+
+  test('Counters display correctly', async () => {
+    await page.goto('https://www.avito.ru/avito-care/eco-impact');
+
+
+    const co2Counter = '.desktop-impact-items-F7T6E > div:nth-child(2)';
+    const waterCounter = '.desktop-impact-items-F7T6E > div:nth-child(4)';
+    const treesCounter = '.desktop-impact-items-F7T6E > div:nth-child(6)';
+
+    const countersSelectors = [co2Counter, waterCounter, treesCounter];
+
+    for (let index = 0; index < countersSelectors.length; index++) {
+      const counterSelector = countersSelectors[index];
+      const screenshotPath = `./output/counter_${index + 1}.png`;
+      // Создаем скриншот текущего счетчика
+      await captureCounterScreenshot(page, counterSelector, screenshotPath);
+    }
+    });
+});
+
+async function captureCounterScreenshot(page: Page, counterSelector: string, screenshotPath: string) {
+  const counterElement = await page.$(counterSelector);
+  if (counterElement) {
+    await counterElement.screenshot({ path: screenshotPath });
+  } else {
+    console.error(`Element with selector ${counterSelector} not found.`);
+  }
+}
